65d764532808862459da1ecc1c4ad2d0f438424c
Aarav <87640437+alphalord18@users.noreply.github.com>
2025-03-16 21:41:11 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -9,7 +9,9 @@ app.use(express.json());
 registerRoutes(app);
 app.use(express.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
+app.use((req, res, next) => 
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
   const start = Date.now();
   const path = req.path;
   let capturedJsonResponse: Record<string, any> | undefined = undefined;
